54ffcb40108da5d595891f206271764ec8d975bd
Oscar Mario <osvamo280788@gmail.com>
2019-06-04 01:48:09 -0600

    Sesion#1

---
+++ AutomatonC/NUnitTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace AutomatonC
+{
+    class NUnitTest
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void Initialize()
+        {
+            driver = new FirefoxDriver();
+        }
+
+        [Test]
+        public void Test01()
+        {
+            driver.Url = "http://127.0.0.1:9000/admin/";
+        }
+
+        [TearDown]
+        public void EndTest()
+        {
+            driver.Close();
+        }
+
+    }
+}
---
+++ AutomatonC/TestCase01.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace AutomatonC
+{
+    class TestCase01
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new FirefoxDriver();
+            driver.Url = "http://127.0.0.1:9000/admin/";
+            driver.Quit();
+        }
+    }
+}
